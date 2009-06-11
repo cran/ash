767bdd9806d0316eb8932b8101c954e7046e59cf
Albrecht Gebhardt <albrecht.gebhardt@uni-klu.ac.at>
2009-06-11 00:00:00 +0000

    version 1.0-11

--- src/ash.f
+++ src/ash.f
@@ -7,7 +7,28 @@ c       "nbin" bins are formed over the interval [a,b)
 c
 c       bin counts returned in array "nc"  -  # pts outside [a,b) = "nskip"
 
-c ##### Copyright 1986 David W. Scott
+c ##### Copyright 1986-2009 David W. Scott
+c #####
+c ##### This program is free software; you can redistribute it and/or
+c ##### modify it under the terms of the GNU General Public License as 
+c ##### published by the Free Software Foundation; either version 2 of 
+c ##### the License, or (at your option) any later version.
+c #####
+c ##### This program is distributed in the hope that it will be useful,
+c ##### but WITHOUT ANY WARRANTY; without even the implied warranty of
+c ##### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+c ##### See the GNU General Public License for more details.
+c #####
+c ##### You should have received a copy of the GNU General Public
+c ##### License along with this program; if not, write to the Free 
+c ##### Software Foundation, Inc.,
+c ##### 51 Franklin St, Fifth Floor, 
+c ##### Boston, MA  02110-1301 USA
+c #####
+c ##### On Debian GNU/Linux systems, the complete text of the GNU
+c ##### General Public License can be found in
+c ##### /usr/share/common-licenses/GPL-2.
+
 
         subroutine bin1 ( x , n , ab , nbin , nc , nskip )
         double precision x(n) , ab(2), d, a, b
