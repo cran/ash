312ef20c0a27db06c254c782070f162ca31e26dd
Albrecht Gebhardt <albrecht.gebhardt@uni-klu.ac.at>
2009-06-11 00:00:00 +0000

    version 1.0-12

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .First.lib <- function(lib, pkg) {
   if(is.R()) {
-    if(version$major==0 && version$minor < 62)
+    if(getRversion()<="0.62")
       stop("This version for R 0.62 or later")
     library.dynam("ash", pkg, lib)
   }
